f31a781f802c3fc7030361734cd0e991a4651fe4
ymoriguchi <yuichiro-moriguchi@nifty.com>
2019-12-13 15:21:40 +0900

    Initial commit

---
+++ Rena.cs
@@ -0,0 +1,482 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Morilib
+{
+    public class Rena<T>
+    {
+        private static readonly Rena<T> instanceRena = new Rena<T>();
+
+        private readonly Func<string, int, T, Result> ignore;
+        private readonly List<string> keys;
+
+        /// <summary>
+        /// Create expression generator.
+        /// </summary>
+        /// <param name="ignore">expression to ignore</param>
+        /// <param name="keys">keywords (operators)</param>
+        public Rena(Func<string, int, T, Result> ignore, string[] keys)
+        {
+            this.ignore = ignore;
+            this.keys = keys == null ? null : new List<string>(keys);
+        }
+
+        private Rena(string ignore, string[] keys)
+        {
+            this.ignore = RE(ignore);
+            this.keys = keys == null ? null : new List<string>(keys);
+        }
+
+        private Rena()
+        {
+            ignore = null;
+            keys = null;
+        }
+
+        /// <summary>
+        /// Create exression generator.
+        /// </summary>
+        /// <param name="regex">regex pattern to ignore</param>
+        /// <param name="keys">keywords (operators)</param>
+        /// <returns>exression generator</returns>
+        public static Rena<T> IgnoreAndKeys(string regex, params string[] keys)
+        {
+            return new Rena<T>(regex, keys);
+        }
+
+        /// <summary>
+        /// Create exression generator.
+        /// </summary>
+        /// <param name="regex">regex pattern to ignore</param>
+        /// <returns>exression generator</returns>
+        public static Rena<T> Ignore(string regex)
+        {
+            return new Rena<T>(regex, null);
+        }
+
+        /// <summary>
+        /// Create exression generator.
+        /// </summary>
+        /// <param name="keys">keywords (operators)</param>
+        /// <returns>exression generator</returns>
+        public static Rena<T> Keys(params string[] keys)
+        {
+            return new Rena<T>((Func<string, int, T, Result>)null, keys);
+        }
+
+        /// <summary>
+        /// Get expression generator without ignoring and keywords.
+        /// </summary>
+        /// <returns>exression generator</returns>
+        public static Rena<T> GetInstance()
+        {
+            return instanceRena;
+        }
+
+        private int SkipSpace(string match, int index)
+        {
+            Result result;
+
+            if(ignore == null || (result = ignore(match, index, default(T))) == null)
+            {
+                return index;
+            }
+            else
+            {
+                return result.LastIndex;
+            }
+        }
+
+        /// <summary>
+        /// A class which has a matching result.
+        /// </summary>
+        public class Result
+        {
+            internal Result(string match, int lastIndex, T attr)
+            {
+                Match = match;
+                LastIndex = lastIndex;
+                Attr = attr;
+            }
+
+            /// <summary>
+            /// matching string
+            /// </summary>
+            public string Match { get; private set; }
+
+            /// <summary>
+            /// last index of matching
+            /// </summary>
+            public int LastIndex { get; private set; }
+
+            /// <summary>
+            /// result attribute
+            /// </summary>
+            public T Attr { get; private set; }
+        }
+
+        /// <summary>
+        /// generates expression to match a string
+        /// </summary>
+        /// <param name="toMatch">string to match</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> Str(string toMatch)
+        {
+            return (match, index, attr) =>
+            {
+                if(index + toMatch.Length <= match.Length && toMatch == match.Substring(index, toMatch.Length))
+                {
+                    return new Result(toMatch, index + toMatch.Length, attr);
+                }
+                else
+                {
+                    return null;
+                }
+            };
+        }
+
+        /// <summary>
+        /// generates expression to match to a pattern
+        /// </summary>
+        /// <param name="toMatch">pattern to match</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> RE(string regex)
+        {
+            Regex pattern = new Regex(regex);
+
+            return (match, index, attr) =>
+            {
+                string toMatch = match.Substring(index);
+                Match matched = pattern.Match(toMatch);
+
+                if (matched.Success && matched.Index == 0)
+                {
+                    return new Result(match.Substring(index, matched.Length), index + matched.Length, attr);
+                }
+                else
+                {
+                    return null;
+                }
+            };
+        }
+
+        /// <summary>
+        /// generates expression to match end of input
+        /// </summary>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> IsEnd()
+        {
+            return (match, index, attr) => index >= match.Length ? new Result("", index, attr) : null;
+        }
+
+        /// <summary>
+        /// generates expression to affect to attribute
+        /// </summary>
+        /// <param name="exp">enclosed expression</param>
+        /// <param name="action">action</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> Action(Func<string, int, T, Result> exp, Func<string, T, T, T> action)
+        {
+            return (match, index, attr) =>
+            {
+                Result result = exp(match, index, attr);
+
+                if(result == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    return new Result(result.Match, result.LastIndex, action(result.Match, result.Attr, attr));
+                }
+            };
+        }
+
+        private Func<string, int, T, Result> Concat(Func<string, int, int> skipSpace, params Func<string, int, T, Result>[] exps)
+        {
+            return (match, index, attr) =>
+            {
+                int indexNew = index;
+                T attrNew = attr;
+
+                foreach (Func<string, int, T, Result> exp in exps)
+                {
+                    Result result = exp(match, indexNew, attrNew);
+
+                    if (result == null)
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        indexNew = skipSpace(match, result.LastIndex);
+                        attrNew = result.Attr;
+                    }
+                }
+                return new Result(match.Substring(index, indexNew - index), indexNew, attrNew);
+            };
+        }
+
+        /// <summary>
+        /// generates expression to match a sequence of expression
+        /// </summary>
+        /// <param name="exps">sequence of expression</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> Concat(params Func<string, int, T, Result>[] exps)
+        {
+            return Concat(SkipSpace, exps);
+        }
+
+        /// <summary>
+        /// generates expression to choice a expression from the arguments.
+        /// </summary>
+        /// <param name="exps">expressions to choice</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> Choice(params Func<string, int, T, Result>[] exps)
+        {
+            return (match, index, attr) =>
+            {
+                foreach (Func<string, int, T, Result> exp in exps)
+                {
+                    Result result = exp(match, index, attr);
+
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+                return null;
+            };
+        }
+
+        /// <summary>
+        /// generates expression which matches if the given expression is not matched
+        /// </summary>
+        /// <param name="exp">expression not to match</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> LookaheadNot(Func<string, int, T, Result> exp)
+        {
+            return (match, index, attr) =>
+            {
+                Result result = exp(match, index, attr);
+
+                if(result == null)
+                {
+                    return new Result("", index, attr);
+                }
+                else
+                {
+                    return null;
+                }
+            };
+        }
+
+        private delegate Func<string, int, T, Result> ILetrec(ILetrec letrec);
+        private delegate Func<string, int, T, Result>[] ILetrecn(ILetrecn letrec);
+        private readonly Func<int, ILetrecn, Func<string, int, T, Result>> FLetrecn =
+            (i, g) => (match, index, attr) => g(g)[i](match, index, attr);
+
+        /// <summary>
+        /// A method which can refer a return values of the function itself.<br>
+        /// This method will be used for defining a expression with recursion.
+        /// </summary>
+        /// <param name="funcs">functions which are return values itself</param>
+        /// <returns>matcher function</returns>
+        public Func<string, int, T, Result> Letrecn(
+            params Func<Func<string, int, T, Result>[], Func<string, int, T, Result>>[] funcs)
+        {
+            ILetrecn f = g => g(g);
+            ILetrecn h = g => {
+                var prms = new List<Func<string, int, T, Result>>();
+                for(int i = 0; i < funcs.Length; i++)
+                {
+                    prms.Add(FLetrecn(i, g));
+                }
+
+                Func<string, int, T, Result>[] args = prms.ToArray();
+                var result = new List<Func<string, int, T, Result>>();
+                foreach(Func<Func<string, int, T, Result>[], Func<string, int, T, Result>> func in funcs)
+                {
+                    result.Add(func(args));
+                }
+                return result.ToArray();
+            };
+            return f(h)[0];
+        }
+
+        /// <summary>
+        /// A method which can refer a return values of the function itself.<br>
+        /// This method will be used for defining a expression with recursion.
+        /// </summary>
+        /// <param name="func">a function which is a return value itself</param>
+        /// <returns>matcher function</returns>
+        public Func<string, int, T, Result> Letrec1(
+            Func<Func<string, int, T, Result>, Func<string, int, T, Result>> func)
+        {
+            ILetrec f = g => g(g);
+            ILetrec h = g => func((match, index, attr) => g(g)(match, index, attr));
+            return f(h);
+        }
+
+        /// <summary>
+        /// A method which can refer a return values of the function itself.<br>
+        /// This method will be used for defining a expression with recursion.
+        /// </summary>
+        /// <param name="func1">a function whose first argument is a return value itself</param>
+        /// <param name="func2">a function whose second argument is a return value itself</param>
+        /// <returns>matcher function</returns>
+        public Func<string, int, T, Result> Letrec2(
+            Func<Func<string, int, T, Result>, Func<string, int, T, Result>, Func<string, int, T, Result>> func1,
+            Func<Func<string, int, T, Result>, Func<string, int, T, Result>, Func<string, int, T, Result>> func2)
+        {
+            ILetrecn f = g => g(g);
+            ILetrecn h = g => {
+                var prm1 = FLetrecn(0, g);
+                var prm2 = FLetrecn(1, g);
+
+                var result = new Func<string, int, T, Result>[2];
+                result[0] = func1(prm1, prm2);
+                result[1] = func2(prm1, prm2);
+                return result;
+            };
+            return f(h)[0];
+        }
+
+        /// <summary>
+        /// A method which can refer a return values of the function itself.<br>
+        /// This method will be used for defining a expression with recursion.
+        /// </summary>
+        /// <param name="func1">a function whose first argument is a return value itself</param>
+        /// <param name="func2">a function whose second argument is a return value itself</param>
+        /// <param name="func3">a function whose third argument is a return value itself</param>
+        /// <returns>matcher function</returns>
+        public Func<string, int, T, Result> Letrec3(
+            Func<Func<string, int, T, Result>, Func<string, int, T, Result>, Func<string, int, T, Result>, Func<string, int, T, Result>> func1,
+            Func<Func<string, int, T, Result>, Func<string, int, T, Result>, Func<string, int, T, Result>, Func<string, int, T, Result>> func2,
+            Func<Func<string, int, T, Result>, Func<string, int, T, Result>, Func<string, int, T, Result>, Func<string, int, T, Result>> func3)
+        {
+            ILetrecn f = g => g(g);
+            ILetrecn h = g => {
+                var prm1 = FLetrecn(0, g);
+                var prm2 = FLetrecn(1, g);
+                var prm3 = FLetrecn(2, g);
+
+                var result = new Func<string, int, T, Result>[3];
+                result[0] = func1(prm1, prm2, prm3);
+                result[1] = func2(prm1, prm2, prm3);
+                result[2] = func3(prm1, prm2, prm3);
+                return result;
+            };
+            return f(h)[0];
+        }
+
+        /// <summary>
+        /// generate expression to match zero or more occurrence
+        /// </summary>
+        /// <param name="exp">expression to repeat</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> ZeroOrMore(Func<string, int, T, Result> exp)
+        {
+            return Letrec1(x => Choice(Concat(exp, x), Str("")));
+        }
+
+        /// <summary>
+        /// generate expression to match one or more occurrence
+        /// </summary>
+        /// <param name="exp">expression to repeat</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> OneOrMore(Func<string, int, T, Result> exp)
+        {
+            return Concat(exp, Letrec1(x => Choice(Concat(exp, x), Str(""))));
+        }
+
+        /// <summary>
+        /// generate expression to match zero or one occurrence
+        /// </summary>
+        /// <param name="exp">expression to match optionally</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> Opt(Func<string, int, T, Result> exp)
+        {
+            return Choice(exp, Str(""));
+        }
+
+        /// <summary>
+        /// generates expression which matches if the given expression is matched but input is not consumed
+        /// </summary>
+        /// <param name="exp">expression to match</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> Lookahead(Func<string, int, T, Result> exp)
+        {
+            return LookaheadNot(LookaheadNot(exp));
+        }
+
+        /// <summary>
+        /// generates expression which matches keyword (operator)
+        /// </summary>
+        /// <param name="key">keyword to match</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> Key(string key)
+        {
+            var skipKeys = new List<Func<string, int, T, Result>>();
+            
+            if(keys == null)
+            {
+                throw new ArgumentException("keys are not set");
+            }
+            foreach(var skipKey in keys)
+            {
+                if(skipKey.Length > key.Length)
+                {
+                    skipKeys.Add(Str(skipKey));
+                }
+            }
+            return Concat(LookaheadNot(Choice(skipKeys.ToArray())), Str(key));
+        }
+
+        /// <summary>
+        /// generates expression which does not match any keywords (operators)
+        /// </summary>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> NotKey()
+        {
+            var skipKeys = new List<Func<string, int, T, Result>>();
+
+            if (keys == null)
+            {
+                throw new ArgumentException("keys are not set");
+            }
+            foreach (var skipKey in keys)
+            {
+                skipKeys.Add(Str(skipKey));
+            }
+            return LookaheadNot(Choice(skipKeys.ToArray()));
+        }
+
+        /// <summary>
+        /// generates expression which matches identifier with succeeding the pattern to ignore
+        /// or keywords (operators)
+        /// </summary>
+        /// <param name="key">identifier to match</param>
+        /// <returns>expression</returns>
+        public Func<string, int, T, Result> EqualsId(string key)
+        {
+            if(ignore == null && keys == null)
+            {
+                return Str(key);
+            }
+            else if(ignore == null)
+            {
+                return Concat((match, index) => index, Str(key), Choice(IsEnd(), LookaheadNot(NotKey())));
+            }
+            else if(keys == null)
+            {
+                return Concat((match, index) => index, Str(key), Choice(IsEnd(), Lookahead(ignore)));
+            }
+            else
+            {
+                return Concat((match, index) => index, Str(key), Choice(IsEnd(), Lookahead(ignore), LookaheadNot(NotKey())));
+            }
+        }
+    }
+}
---
+++ RenaTest.cs
@@ -0,0 +1,286 @@
+using NUnit.Framework;
+using System;
+
+namespace Morilib
+{
+    public class RenaTest
+    {
+        private void Match(Func<string, int, int, Rena<int>.Result> exp, string toMatch, string matched, int index)
+        {
+            Rena<int>.Result result = exp(toMatch, 0, default(int));
+            Assert.AreEqual(matched, result.Match);
+            Assert.AreEqual(index, result.LastIndex);
+        }
+
+        private void Match(Func<string, int, int, Rena<int>.Result> exp, string toMatch, int startIndex, string matched, int index)
+        {
+            Rena<int>.Result result = exp(toMatch, startIndex, default(int));
+            Assert.AreEqual(matched, result.Match);
+            Assert.AreEqual(index, result.LastIndex);
+        }
+
+        private void MatchAttr(Func<string, int, int, Rena<int>.Result> exp, string toMatch, string matched, int index, int initAttr, int attr)
+        {
+            Rena<int>.Result result = exp(toMatch, 0, initAttr);
+            Assert.AreEqual(matched, result.Match);
+            Assert.AreEqual(index, result.LastIndex);
+            Assert.AreEqual(attr, result.Attr);
+        }
+
+        private void NoMatch(Func<string, int, int, Rena<int>.Result> exp, string toMatch)
+        {
+            Rena<int>.Result result = exp(toMatch, 0, default(int));
+            Assert.IsNull(result);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestStr()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.Str("abcd");
+
+            Match(matcher, "abcde", "abcd", 4);
+            NoMatch(matcher, "abcx");
+            NoMatch(matcher, "abc");
+            NoMatch(matcher, "");
+        }
+
+        [Test]
+        public void TestRE()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.RE("[a-z]+");
+
+            Match(matcher, "abcde", "abcde", 5);
+            Match(matcher, "abcx1", "abcx", 4);
+            NoMatch(matcher, "123");
+            NoMatch(matcher, "");
+        }
+
+        [Test]
+        public void TestIsEnd()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.IsEnd();
+
+            Match(matcher, "a", 1, "", 1);
+            NoMatch(matcher, "123");
+        }
+
+        [Test]
+        public void TestAction()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.Action(r.Action(r.RE("[0-9]+"), (match, syn, inh) => int.Parse(match)), (match, syn, inh) => inh - syn);
+
+            MatchAttr(matcher, "27", "27", 2, 28, 1);
+        }
+
+        [Test]
+        public void TestConcat()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.Concat(r.Str("a"), r.Str("c"), r.Str("e"));
+
+            Match(matcher, "ace", "ace", 3);
+            NoMatch(matcher, "bce");
+            NoMatch(matcher, "ade");
+            NoMatch(matcher, "acf");
+            NoMatch(matcher, "cea");
+            NoMatch(matcher, "ac");
+            NoMatch(matcher, "");
+        }
+
+        [Test]
+        public void TestChoice()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.Choice(r.Str("a"), r.Str("c"), r.Str("e"));
+
+            Match(matcher, "ace", "a", 1);
+            Match(matcher, "ce", "c", 1);
+            Match(matcher, "e", "e", 1);
+            NoMatch(matcher, "b");
+            NoMatch(matcher, "");
+        }
+
+        [Test]
+        public void TestLookaheadNot()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.Concat(r.LookaheadNot(r.Str("ab")), r.RE("[a-z]+"));
+
+            Match(matcher, "accde", "accde", 5);
+            NoMatch(matcher, "abcde");
+        }
+
+        [Test]
+        public void TestLetrec1()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.Letrec1(x => r.Choice(r.Concat(r.Str("("), x, r.Str(")")), r.Str("")));
+
+            Match(matcher, "((()))", "((()))", 6);
+            Match(matcher, "((())))", "((()))", 6);
+            Match(matcher, "((())", "", 0);
+        }
+
+        [Test]
+        public void TestLetrec2()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.Letrec2(
+                (x, y) => r.Choice(r.Concat(r.Str("("), y, r.Str(")")), r.Str("")),
+                (x, y) => r.Concat(r.Str("["), x, r.Str("]")));
+
+            Match(matcher, "([([])])", "([([])])", 8);
+            Match(matcher, "([([])]", "", 0);
+        }
+
+        [Test]
+        public void TestLetrec3()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.Letrec3(
+                (x, y, z) => r.Choice(r.Concat(r.Str("("), y, r.Str(")")), r.Str("")),
+                (x, y, z) => r.Concat(r.Str("["), z, r.Str("]")),
+                (x, y, z) => r.Concat(r.Str("{"), x, r.Str("}")));
+
+            Match(matcher, "([{([{}])}])", "([{([{}])}])", 12);
+            Match(matcher, "([{([{}])}]", "", 0);
+        }
+
+        [Test]
+        public void TestZeroOrMore()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.ZeroOrMore(r.Str("a"));
+
+            Match(matcher, "aaaa", "aaaa", 4);
+            Match(matcher, "a", "a", 1);
+            Match(matcher, "", "", 0);
+        }
+
+        [Test]
+        public void TestOneOrMore()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.OneOrMore(r.Str("a"));
+
+            Match(matcher, "aaaa", "aaaa", 4);
+            Match(matcher, "a", "a", 1);
+            NoMatch(matcher, "");
+        }
+
+        [Test]
+        public void TestOpt()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.Opt(r.Str("a"));
+
+            Match(matcher, "aaaa", "a", 1);
+            Match(matcher, "a", "a", 1);
+            Match(matcher, "", "", 0);
+        }
+
+        [Test]
+        public void TestLookahead()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.Concat(r.Lookahead(r.Str("ab")), r.RE("[a-z]+"));
+
+            Match(matcher, "abcde", "abcde", 5);
+            NoMatch(matcher, "accde");
+        }
+
+        [Test]
+        public void TestKey()
+        {
+            Rena<int> r = Rena<int>.Keys("+", "++", "--");
+            var matcher = r.Key("+");
+
+            Match(matcher, "+", "+", 1);
+            Match(matcher, "+1", "+", 1);
+            NoMatch(matcher, "++");
+        }
+
+        [Test]
+        public void TestNotKey()
+        {
+            Rena<int> r = Rena<int>.Keys("+", "++", "--");
+            var matcher = r.NotKey();
+
+            Match(matcher, "-", "", 0);
+            NoMatch(matcher, "++");
+            NoMatch(matcher, "--");
+            NoMatch(matcher, "+");
+        }
+
+        [Test]
+        public void TestEqualsId1()
+        {
+            Rena<int> r = Rena<int>.GetInstance();
+            var matcher = r.EqualsId("key");
+
+            Match(matcher, "key", "key", 3);
+            Match(matcher, "key+", "key", 3);
+            Match(matcher, "key ", "key", 3);
+            Match(matcher, "keys", "key", 3);
+            NoMatch(matcher, "not");
+        }
+
+        [Test]
+        public void TestEqualsId2()
+        {
+            Rena<int> r = Rena<int>.Keys("+", "++", "--");
+            var matcher = r.EqualsId("key");
+
+            Match(matcher, "key", "key", 3);
+            Match(matcher, "key+", "key", 3);
+            NoMatch(matcher, "key ");
+            NoMatch(matcher, "keys");
+            NoMatch(matcher, "not");
+        }
+
+        [Test]
+        public void TestEqualsId3()
+        {
+            Rena<int> r = Rena<int>.Ignore("\\s+");
+            var matcher = r.EqualsId("key");
+
+            Match(matcher, "key", "key", 3);
+            NoMatch(matcher, "key+");
+            Match(matcher, "key ", "key", 3);
+            NoMatch(matcher, "keys");
+            NoMatch(matcher, "not");
+        }
+
+        [Test]
+        public void TestEqualsId4()
+        {
+            Rena<int> r = Rena<int>.IgnoreAndKeys("\\s+", "+", "++", "--");
+            var matcher = r.EqualsId("key");
+
+            Match(matcher, "key", "key", 3);
+            Match(matcher, "key+", "key", 3);
+            Match(matcher, "key ", "key", 3);
+            NoMatch(matcher, "keys");
+            NoMatch(matcher, "not");
+        }
+
+        [Test]
+        public void TestSkipSpace()
+        {
+            Rena<int> r = Rena<int>.Ignore("\\s+");
+            var matcher = r.ZeroOrMore(r.RE("[a-z]+"));
+
+            Match(matcher, "aa aa  aa", "aa aa  aa", 9);
+            Match(matcher, "a", "a", 1);
+        }
+    }
+}
